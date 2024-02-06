5ea675d7dedf1def58e7942a75d637f53ec796a4
Javier Vivas <javivasv@gmail.com>
2024-02-06 16:28:09 -0500

    Dashboard structure for mobile

--- src/containers/About.tsx
+++ src/containers/About.tsx
@@ -5,10 +5,10 @@ import SecondaryContent from "../components/About/SecondaryContent";
 function About() {
   return (
     <Grid className="h-100 w-100" container flexDirection={"row"}>
-      <Grid className="main-content" container item flexDirection={"column"} xs={8}>
+      <Grid className="main-content" container item flexDirection={"column"} xs={12} md={8}>
         <MainContent />
       </Grid>
-      <Grid className="secondary-content" container item flexDirection={"column"} xs={4}>
+      <Grid className="secondary-content" container item flexDirection={"column"} xs={12} md={4}>
         <SecondaryContent />
       </Grid>
     </Grid>
