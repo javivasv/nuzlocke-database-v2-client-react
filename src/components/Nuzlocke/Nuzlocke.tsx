70d315f5ef69a9317179da5fce73c1c90341c2fe
Javier Vivas <javivasv@gmail.com>
2024-02-10 11:13:37 -0500

    Nuzlocke tab setup

--- src/components/Nuzlocke/Nuzlocke.tsx
+++ src/components/Nuzlocke/Nuzlocke.tsx
@@ -2,9 +2,10 @@ import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../store/store";
-import { CustomError } from '../../interfaces/interfaces';
-import { Grid, Card } from "@mui/material";
 import { fetchNuzlocke, setNuzlocke } from "../../store/nuzlockes/nuzlockesSlice";
+import { CustomError } from '../../interfaces/interfaces';
+import { Grid, Card, Tabs, Tab } from "@mui/material";
+import LoadingRow from '../LoadingRow';
 
 interface Props {
   ValidateError: (e: CustomError) => void;
@@ -16,6 +17,8 @@ function Nuzlockes(props: Props) {
   const nuzlocke = useSelector((state: RootState) => state.nuzlockes.nuzlocke);
 
   const [loading, setLoading] = useState(false);
+
+  const [tab, setTab] = useState(0);
 
   useEffect(() => {
     setLoading(true);
@@ -39,14 +42,32 @@ function Nuzlockes(props: Props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const HandleTabChange = (val: number) => {
+    setTab(val);
+  }
+
   return (
     <Grid container flexDirection={"row"}>
       <Card className="main-content-card">
         <Grid container flexDirection={"row"}>
           <Grid container item flexDirection={"column"}>
-            <span>
-              { loading }
-            </span>
+            <Grid container flexDirection={"row"}>
+              <Tabs value={tab} onChange={(_, val) => HandleTabChange(val)} aria-label="basic tabs example">
+                <Tab label="Pokemon" />
+              </Tabs>
+            </Grid>
+            {
+              loading &&
+              <Grid container flexDirection={"row"}>
+                <LoadingRow />
+              </Grid>
+            }
+            {
+              !loading &&
+              <Grid container flexDirection={"row"}>
+                LOADED
+              </Grid>
+            }
           </Grid>
         </Grid>
       </Card>
